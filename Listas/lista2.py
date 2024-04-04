bddf0b8911bcc31a371747900ca01301effe768c
MercioGiaretta <128699194+MercioGiaretta@users.noreply.github.com>
2024-04-04 09:40:12 -0300

    Add files via upload

--- Listas/lista2.py
+++ Listas/lista2.py
@@ -1,3 +1,5 @@
+import os
+
 def exibirLista(lista):
     print("Lista atual: ", lista)
 
@@ -21,6 +23,34 @@ def gravarLista(lista):
             arquivo.write(item + "\n")
     print("LISTA GRAVADO COM SUCESSO", {nome_arq})
 
+    lista.sort(reversed = True)
+    print("Lista ordenada com sucesso")
+
+def listar_arquivos():
+    diretorio = os.getcwd()
+    arquivos = os.listdir(diretorio)
+    print("Os arquivos desse diretorio são:")
+    for lista_arquivos in arquivos:
+        print(lista_arquivos) 
+
+def carregar_arquivo(lista):
+
+    nome_arquivo = input("Digite o nome do arquivo para carregar a lista:")
+    try:
+        with open (nome_arquivo, "r") as arquivo:
+            lista.clear()
+            for linha in arquivo:
+                lista.append(linha.strip())
+        print("Lista carregada com sucesso do arquivo" , nome_arquivo)
+    except FileNotFoundError:
+        print("O arquivo não foi encontrado.")
+    except Exception as e:
+        print("Ocorreu um erro." , e)
+
+def ordenar_lista(lista):
+    lista.sort(reverse = True)
+    print("Lista ordenada com sucesso!")
+
 
 def principal():
     lista = []
@@ -32,7 +62,10 @@ def principal():
         print("2 - Excluir Item")
         print("3 - Exibir Lista")
         print("4 - Gravar Lista")
-        print("5 - Sair")
+        print("5 - Listar Arquivos")
+        print("6 - Carregar Lista")
+        print("7 - Ordenar Lista")
+        print("8 - Sair")
 
         opcoes = input("Digite o número da opção desejada: ")
 
@@ -45,13 +78,24 @@ def principal():
         elif opcoes == "4":
             gravarLista(lista)
         elif opcoes == "5":
+            listar_arquivos()
+        elif opcoes == "6":
+            carregar_arquivo(lista)
+        elif opcoes == "7":
+            ordenar_lista(lista)
+        elif opcoes == "8":
             continuar = False
+
+
+
         else:
             print("Opção inválida. Digite novamente.")
 
         print()
 
     print("Programa Encerrado.")
+
+    
 
 if __name__ == "__main__":
     principal()

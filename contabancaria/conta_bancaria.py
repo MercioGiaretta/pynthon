bbe494fd098cdf3bcf6ebc7e0b9c1437eca3009d
MercioGiaretta <128699194+MercioGiaretta@users.noreply.github.com>
2024-04-11 09:29:17 -0300

    Add files via upload

--- contabancaria/conta_bancaria.py
+++ contabancaria/conta_bancaria.py
@@ -3,5 +3,29 @@ class ContaBancaria:
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
-    def deposita(sself,valor):
+
+    def depositar(self,valor):
         self.saldo =+ valor
+    
+    def exibir_detalhes(self):
+        print("Número da Conta:", self.numero)
+        print("Titular:", self.titular)
+        print("Saldo:", self.saldo)
+
+    def sacar(self,valor):
+        if self.saldo >=  valor:
+            self.saldo -= valor
+        else:
+            print("Lhe falta kwanzas")
+
+numero_acc = input("Digite o número da sua conta: ")
+titular_acc = input("Digite o número do titular: ")
+saldo_acc = float(input("Digite o Saldo Atual: ").replace(",","."))
+
+conta_do_mercio = ContaBancaria(numero_acc, titular_acc, saldo_acc)
+deposito = float(input("Digite o valor a ser depositado ").replace(",","."))
+saque = float(input("Digite o valor a ser sacado ").replace(",","."))
+
+conta_do_mercio.depositar(4100)
+conta_do_mercio.sacar(200)
+conta_do_mercio.exibir_detalhes()
